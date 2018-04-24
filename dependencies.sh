52fe87e66c061d6e88735add3db4986c673b04fe
ratulbanerjee <33754328+ratulbanerjee@users.noreply.github.com>
2018-04-24 22:09:36 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -7,6 +7,7 @@ pip install awscli
 pip install django
 pip install boto3
 pip install requests
+pwd
 BUILD_ID=dontKillMe nohup python ./hello_world_pipeline/manage.py runserver ec2-34-215-136-238.us-west-2.compute.amazonaws.com &
 
 echo "dependencies are installed"
